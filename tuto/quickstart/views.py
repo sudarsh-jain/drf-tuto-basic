30dc129b1113ca34f47c4fb9b0dc2558f891dd50
Sudharsan Rajendhar <sudharsan.r@predigle.com>
2024-08-07 15:16:23 +0530

    django rest framework: get started tutorial

--- tuto/quickstart/views.py
+++ tuto/quickstart/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
+from tuto.quickstart.serializers import GroupSerializer, UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.order_by("-name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
